783aca582663f883af40b92ee2837f7dcc4baba4
angel <actolentino2@student.hau.edu.ph>
2026-03-10 20:52:25 +0800

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
-import Footer from './components/Footer';
+import Footer from './components/footer';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
